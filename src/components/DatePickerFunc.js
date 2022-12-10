911883e6e38713b777c9b89a50748f473758b904
rin-gordienko <rin.gordienko@gmail.com>
2022-12-10 13:24:25 +0200

    changed date

--- src/components/DatePickerFunc.js
+++ src/components/DatePickerFunc.js
@@ -7,6 +7,7 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function DatePickerFunc() {
   const [value, setValue] = React.useState(null);
+  console.log(value);
 
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
